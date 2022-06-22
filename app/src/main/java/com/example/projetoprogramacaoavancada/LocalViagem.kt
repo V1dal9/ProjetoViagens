d2c44002a81df2b421149d631aee294311cb1736
Ana Vidal <raquelvidal99@hotmail.com>
2022-06-22 12:00:41 +0100

    Correção da relação da tabela LocalViagem.

--- app/src/main/java/com/example/projetoprogramacaoavancada/LocalViagem.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/LocalViagem.kt
@@ -1,15 +1,32 @@
 package com.example.projetoprogramacaoavancada
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 class LocalViagem(
-    var idViagem : Long,
+    var idLocalViagem : Long = -1,
+    var idInfoViagem : Long,
     val idLocal : Long
 ) {
     fun toContentValues() : ContentValues {
         val valoresLocal = ContentValues()
-        valoresLocal.put(Tabela_Info_Viagem_Bilhete.CAMPO_ID, idViagem)
+        valoresLocal.put(Tabela_Info_Viagem_Bilhete.CAMPO_ID, idInfoViagem)
         valoresLocal.put(TabelaLocal.LOCAL_ID, idLocal)
         return valoresLocal
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor):LocalViagem {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posInfoViagem = cursor.getColumnIndex(TabelaLocalViagem.INFOVIAGEM_ID)
+            val posLocal = cursor.getColumnIndex(TabelaLocalViagem.LOCAL_ID)
+
+            val id = cursor.getLong(posId)
+            val infoViagemId = cursor.getLong(posInfoViagem)
+            val localId = cursor.getLong(posLocal)
+
+            return LocalViagem(id, infoViagemId, localId)
+        }
     }
 }
--- app/src/main/java/com/example/projetoprogramacaoavancada/TabelaLocalViagem.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/TabelaLocalViagem.kt
@@ -1,17 +1,39 @@
 package com.example.projetoprogramacaoavancada
 
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteQueryBuilder
 import android.provider.BaseColumns
 
-class TabelaLocalViagem(db : SQLiteDatabase): TabelaBD(db, NOME_DESTINO) {
+class TabelaLocalViagem(db : SQLiteDatabase): TabelaBD(db, NOME) {
     override fun cria() {
     db.execSQL("CREATE TABLE $nome(${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
             "$LOCAL_ID INTEGER NOT NULL, " +
-            "FOREIGN KEY($LOCAL_ID) REFERENCES ${Tabela_Info_Viagem_Bilhete.NOME}(${BaseColumns._ID})" +
-            ")")
+            "FOREIGN KEY($INFOVIAGEM_ID) REFERENCES ${Tabela_Info_Viagem_Bilhete.NOME}(${BaseColumns._ID})ON DELETE RESTRICT," +
+            "FOREIGN KEY($LOCAL_ID) REFERENCES ${TabelaLocal.NOME}(${BaseColumns._ID})ON DELETE RESTRICT)")
+    }
+
+    override fun query(
+        columns: Array<String>,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        groupBy: String?,
+        having: String?,
+        orderBy: String?
+    ): Cursor {
+        val queryBuilder = SQLiteQueryBuilder()
+        queryBuilder.tables = "$NOME INNER JOIN ${Tabela_Info_Viagem_Bilhete.NOME} ON ${Tabela_Info_Viagem_Bilhete.CAMPO_ID} = $INFOVIAGEM_ID"
+        queryBuilder.tables = "$NOME INNER JOIN ${TabelaLocal.NOME} ON ${TabelaLocal.LOCAL_ID} = $LOCAL_ID"
+
+        return queryBuilder.query(db, columns, selection, selectionArgs, groupBy, having, orderBy)
     }
     companion object{
-        const val NOME_DESTINO = "Destino"
-        const val LOCAL_ID = "travel_id"
+        const val NOME = "LocalViagem"
+
+        const val CAMPO_ID = "$NOME.${BaseColumns._ID}"
+        const val INFOVIAGEM_ID = "infoViagemId"
+        const val LOCAL_ID = "travelId"
+
+        val TODAS_COLUNAS = arrayOf(CAMPO_ID, INFOVIAGEM_ID, LOCAL_ID)
     }
 }
