13cacc2b52e6b53bcea4f082b91f28d66ee39607
Ana Vidal <raquelvidal99@hotmail.com>
2022-06-22 11:40:59 +0100

    Correção da relação da tabela Local.

--- app/src/main/java/com/example/projetoprogramacaoavancada/Local.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/Local.kt
@@ -1,17 +1,33 @@
 package com.example.projetoprogramacaoavancada
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 class Local(
-    var id : Long,
+    var id : Long = -1,
     val localOrigem : String,
     val localDestino : String
 ) {
     fun toContentValues() : ContentValues{
-        val valoresOrigem = ContentValues()
-        valoresOrigem.put(TabelaLocal.LOCAL_ID, id)
-        valoresOrigem.put(TabelaLocal.LOCAL_ORIGEM, localOrigem)
-        valoresOrigem.put(TabelaLocal.LOCAL_DESTINO, localDestino)
-        return valoresOrigem
+        val valoresLocal = ContentValues()
+
+        valoresLocal.put(TabelaLocal.LOCAL_ORIGEM, localOrigem)
+        valoresLocal.put(TabelaLocal.LOCAL_DESTINO, localDestino)
+        return valoresLocal
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor): Local{
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posOrigem = cursor.getColumnIndex(TabelaLocal.LOCAL_ORIGEM)
+            val posDestino = cursor.getColumnIndex(TabelaLocal.LOCAL_DESTINO)
+
+            val id = cursor.getLong(posId)
+            val origem = cursor.getString(posOrigem)
+            val destino = cursor.getString(posDestino)
+
+            return Local(id, origem, destino)
+        }
     }
 }
--- app/src/main/java/com/example/projetoprogramacaoavancada/TabelaLocal.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/TabelaLocal.kt
@@ -1,20 +1,36 @@
 package com.example.projetoprogramacaoavancada
 
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteQueryBuilder
 import android.provider.BaseColumns
 
-class TabelaLocal (db: SQLiteDatabase):TabelaBD(db, NOME_LOCAL) {
+class TabelaLocal (db: SQLiteDatabase):TabelaBD(db, NOME) {
     override fun cria() {
         db.execSQL("CREATE TABLE $nome(${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "$NOME_LOCAL TEXT NOT NULL, " +
                 "$LOCAL_ORIGEM TEXT NOT NULL, " +
-                "$LOCAL_DESTINO TEXT NOT NULL, " +
-                "$LOCAL_ID INTEGER NOT NULL) ")
+                "$LOCAL_DESTINO TEXT NOT NULL)")
+    }
+
+    override fun query(
+        columns: Array<String>,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        groupBy: String?,
+        having: String?,
+        orderBy: String?
+    ): Cursor {
+        val queryBuilder = SQLiteQueryBuilder()
+
+        queryBuilder.tables = "$NOME INNER JOIN ${TabelaLocalViagem.NOME_DESTINO} ON ${TabelaLocalViagem.LOCAL_ID} = $LOCAL_ID"
+        return queryBuilder.query(db, columns, selection, selectionArgs, groupBy, having, orderBy)
     }
     companion object{
+        const val NOME = "local"
+
         const val LOCAL_ID = "id"
-        const val NOME_LOCAL = "local"
         const val LOCAL_ORIGEM = "origem"
         const val LOCAL_DESTINO = "destino"
+        val TODAS_COLUNAS = arrayOf(LOCAL_ID, LOCAL_ORIGEM, LOCAL_DESTINO)
     }
 }
