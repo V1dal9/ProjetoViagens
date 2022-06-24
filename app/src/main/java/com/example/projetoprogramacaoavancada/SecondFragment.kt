16931efca9d70d8fb433c89eecd0ef1191342594
Ana Vidal <raquelvidal99@hotmail.com>
2022-06-25 00:25:05 +0100

    iniciação da criação do layout da app

--- app/src/main/java/com/example/projetoprogramacaoavancada/SecondFragment.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/SecondFragment.kt
@@ -1,20 +1,26 @@
 package com.example.projetoprogramacaoavancada
 
+import android.database.Cursor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.projetoprogramacaoavancada.databinding.FragmentSecondBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class SecondFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
 
     private var _binding: FragmentSecondBinding? = null
-
+    private var adapterViagem : AdapterViagem? = null
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -32,13 +38,51 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
+        adapterViagem = AdapterViagem(this)
+        binding.recyclerViewLista.adapter = adapterViagem
+        binding.recyclerViewLista.layoutManager = LinearLayoutManager(requireContext())
+        val activity = activity as MainActivity
+        activity.fragment = this
+        activity.idMenuAtual = R.menu.menu_main
+        /*binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+        }*/
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        return CursorLoader(
+            requireContext(),
+            ContentProviderViagem.ENDERECO_PASSAGEIRO,
+            Tabela_Passageiro.TODAS_COLUNAS,
+            null,
+            null,
+            "${Tabela_Passageiro}"
+        )
+    }
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        adapterViagem!!.cursor = data
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        adapterViagem!!.cursor = null
+    }
+
+    fun processaOpcaoMenu(item: MenuItem) : Boolean =
+        when(item.itemId){
+            R.id.action_inserir -> {
+                findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+                true
+            }
+            R.id.action_edit -> true
+            R.id.action_eliminar -> true
+            else -> false
+
+        }
 }
