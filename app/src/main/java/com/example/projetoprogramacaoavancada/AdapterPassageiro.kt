af25745934275cd332a6e228b1deb4bd00274166
Ana Vidal <raquelvidal99@hotmail.com>
2022-07-16 20:20:04 +0100

    declarado o cursor, e agora inserir os campos da tabela passageiro, nas respetivas textViews a irão ser inseridas no fragmente_passageiro

--- app/src/main/java/com/example/projetoprogramacaoavancada/AdapterPassageiro.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/AdapterPassageiro.kt
@@ -1,5 +1,23 @@
 package com.example.projetoprogramacaoavancada
 
-class AdapterPassageiro {
+import android.database.Cursor
+
+class AdapterPassageiro(val fragment: PassageiroFragment) {
+    var cursor : Cursor? = null
+    get() = field
+    set(value){
+        if(field != value){
+            field = value
+            //notifyDataSetChanged() falta implementar viewHolder
+        }
+    }
+
+    var passageiro : Passageiro? = null
+        get() = field
+        set(value){
+            field = value
+
+        }
+    //var viewHolderSelecionado : View
 
 }
