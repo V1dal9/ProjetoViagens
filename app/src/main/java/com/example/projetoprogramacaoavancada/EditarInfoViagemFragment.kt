22755095ec4fa3d4d7abcb960150a7d524d567ed
Ana Vidal <raquelvidal99@hotmail.com>
2022-07-21 13:52:23 +0100

    Removi função onCreate, pois não era necessária para o trabalho. Implementada função processaOpçãoMenu com as ações guardar e cancelar, também foi implementada a função voltarLista por causa da opção cancelar.

--- app/src/main/java/com/example/projetoprogramacaoavancada/EditarInfoViagemFragment.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/EditarInfoViagemFragment.kt
@@ -1,15 +1,19 @@
 package com.example.projetoprogramacaoavancada
 
 import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SimpleCursorAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
+import androidx.navigation.fragment.findNavController
 import com.example.projetoprogramacaoavancada.databinding.FragmentEditarViagemBinding
 
 
@@ -20,16 +24,13 @@ class EditarInfoViagemFragment :Fragment(), LoaderManager.LoaderCallbacks<Cursor
     // This property is only valid between onCreateView and
     // onDestroyView
     private val binding get() = _binding!!
-
-    override fun onCreate(savedInstanceState : Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var viagem : Lista_Viagem? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentEditarViagemBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -72,21 +73,202 @@ class EditarInfoViagemFragment :Fragment(), LoaderManager.LoaderCallbacks<Cursor
           intArrayOf(android.R.id.text1),
           0
         )
-        binding.spinnerGenero.adapter = adapterGenero
+        binding.spinnerPassageiro.adapter = adapterGenero
 
     }
 
     override fun onLoaderReset(loader: Loader<Cursor>) {
-        binding.spinnerGenero.adapter = null
+        binding.spinnerPassageiro.adapter = null
     }
 
-    /*fun processaOpçãoMenu(item: MenuItem) : Boolean =
+    fun processaOpcaoMenu(item: MenuItem) : Boolean =
        when(item.itemId){
-           R.id.ac
-       } */
+           R.id.action_guardar -> {
+               guardar()
+               true
+           }
+           R.id.action_cancelar -> {
+               voltarListaViagem()
+               true
+           }
+           else -> false
+       }
+
+    private fun guardar(){
+        val nomeLista = binding.editTextNomeLista.text.toString()
+        if(nomeLista.isBlank()){
+            binding.editTextNomeLista.error = "Preencha o Nome da lista"
+            binding.editTextNomeLista.requestFocus()
+            return
+        }
+
+        val Roupa = binding.editTextRoupa.text.toString()
+        if(Roupa.isBlank()){
+            binding.editTextRoupa.error = "Preencha o campo roupa"
+            binding.editTextRoupa.requestFocus()
+            return
+        }
+
+        val acessorios = binding.editTextAcessorios.text.toString()
+        if(acessorios.isBlank()){
+            binding.editTextAcessorios.error = "Preencha o campo acessorios"
+            binding.editTextAcessorios.requestFocus()
+            return
+        }
+
+        val eletronicos = binding.editTextEletronicos.text.toString()
+        if(eletronicos.isBlank()){
+            binding.editTextEletronicos.error = "Preencha o campo eletronicos"
+            binding.editTextEletronicos.requestFocus()
+            return
+        }
+
+        val higiene = binding.editTextHigiene.text.toString()
+        if(higiene.isBlank()){
+            binding.editTextHigiene.error = "Preencha o campo higiene"
+            binding.editTextHigiene.requestFocus()
+            return
+        }
+
+        val calcado = binding.editTextCalcado.text.toString()
+        if(calcado.isBlank()){
+            binding.editTextCalcado.error = "Preencha o campo calçado"
+            binding.editTextCalcado.requestFocus()
+            return
+        }
+
+        val idPassageiro = binding.spinnerPassageiro.selectedItemId
+        if(idPassageiro == Spinner.INVALID_ROW_ID){
+            binding.textViewPassageiro.error = "Escolha um passageiro"
+            binding.spinnerPassageiro.requestFocus()
+            return
+        }
+
+        val genero = binding.editTextGenero.text.toString()
+        if(genero.isBlank()){
+            binding.editTextCalcado.error = "Preencha o campo genero"
+            binding.editTextGenero.requestFocus()
+            return
+        }
+
+        val idade = binding.editTextIdade.text.toString()
+        if(idade.isBlank()){
+            binding.editTextIdade.error = "Preencha o campo idade"
+            binding.editTextIdade.requestFocus()
+            return
+        }
+
+
+
+        val viagemGuardada =
+            if(viagem == null){
+                //inserirViagem(nomeLista, Roupa, acessorios, eletronicos, higiene, calcado, idPassageiro, genero, idade, )
+            }else{
+                //alterarViagem(nomeLista, Roupa, acessorios, eletronicos, higiene, calcado, idPassageiro, genero, idade, )
+            }
+    }
+
+    private fun alterarViagem(
+        nomeLista: String,
+        roupa: String,
+        acessorios: String,
+        eletronicos: String,
+        higiene: String,
+        calcado: String,
+        idPassageiro: Long,
+        nomePassageiro: String,
+        generoPassageiro: String,
+        idadePassageiro: Long,
+        idInfoViagemBilhete: Long,
+        dataInicio: String,
+        dataFim: String,
+        localOrigem: String,
+        localDestino: String,
+        tipoMala: String,
+        classViagem: String,
+        passageiro: Passageiro
+        ): Boolean{
+            val viagem = Lista_Viagem(
+                nomeLista,
+                roupa,
+                acessorios,
+                eletronicos,
+                higiene,
+                calcado,
+                Passageiro(
+                    id = idPassageiro,
+                    nome = nomePassageiro,
+                    genero = generoPassageiro,
+                    idade = idadePassageiro),
+                InfoViagemBilhete(
+                    id = idInfoViagemBilhete,
+                    dataFim = dataFim,
+                    dataInicio = dataInicio,
+                    localDestino = localDestino,
+                    localOrigem = localOrigem,
+                    tipoMala = tipoMala,
+                    classViagem = classViagem,
+                    Passageiro = passageiro
+                    )
+            )
+
+        val enderecoViagem = Uri.withAppendedPath(ContentProviderViagem.ENDERECO_LISTA, "${this.viagem!!.id}")
+
+        val registosAlterados = requireActivity().contentResolver.update(enderecoViagem, viagem.toContentValues(), null, null)
+
+        return registosAlterados == 1
+    }
+
+    private fun inserirViagem(
+        nomeLista: String,
+        roupa: String,
+        acessorios: String,
+        eletronicos: String,
+        higiene: String,
+        calcado: String,
+        idPassageiro: Long,
+        nomePassageiro: String,
+        generoPassageiro: String,
+        idadePassageiro: Long,
+        idInfoViagemBilhete: Long,
+        dataInicio: String,
+        dataFim: String,
+        localOrigem: String,
+        localDestino: String,
+        tipoMala: String,
+        classViagem: String,
+        passageiro: Passageiro
+    ): Boolean{
+        val viagem = Lista_Viagem(nomeLista,
+            roupa,
+            acessorios,
+            eletronicos,
+            higiene,
+            calcado,
+            Passageiro(
+                id = idPassageiro,
+                nome = nomePassageiro,
+                genero = generoPassageiro,
+                idade = idadePassageiro),
+            InfoViagemBilhete(
+                id = idInfoViagemBilhete,
+                dataFim = dataFim,
+                dataInicio = dataInicio,
+                localDestino = localDestino,
+                localOrigem = localOrigem,
+                tipoMala = tipoMala,
+                classViagem = classViagem,
+                Passageiro = passageiro)
+        )
+
+        val enderecoViagemInserir = requireActivity().contentResolver.insert(ContentProviderViagem.ENDERECO_LISTA, viagem.toContentValues())
+
+        return enderecoViagemInserir != null
+    }
+
 
     private fun voltarListaViagem(){
-        //
+        findNavController().navigate(R.id.action_EditarViagemFragment_to_ListaViagem)
     }
 
 
