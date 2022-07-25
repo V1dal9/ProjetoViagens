39684900d22281504c3f8c55081c09a94a48dbc1
Ana Vidal <raquelvidal99@hotmail.com>
2022-07-25 01:44:48 +0100

    Implementada class ViewHolderPassageiro, fun OnClick, desseleciona, seleciona, onCreatViewKolder, onBindingViewHolder e getItemCount.

--- app/src/main/java/com/example/projetoprogramacaoavancada/AdapterPassageiro.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/AdapterPassageiro.kt
@@ -1,23 +1,75 @@
 package com.example.projetoprogramacaoavancada
 
 import android.database.Cursor
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
-class AdapterPassageiro(val fragment: PassageiroFragment) {
+class AdapterPassageiro(val fragment: PassageiroFragment2) : RecyclerView.Adapter<AdapterPassageiro.ViewHolderPassageiro>(){
     var cursor : Cursor? = null
     get() = field
     set(value){
         if(field != value){
             field = value
-            //notifyDataSetChanged() falta implementar viewHolder
+            notifyDataSetChanged()
         }
     }
+    var viewHolderSelecionado : ViewHolderPassageiro? = null
 
-    var passageiro : Passageiro? = null
-        get() = field
-        set(value){
-            field = value
+    inner class ViewHolderPassageiro(itemPassageiro: View) : RecyclerView.ViewHolder(itemPassageiro), View.OnClickListener{
 
+        var textViewNomePassageiro = itemPassageiro.findViewById<TextView>(R.id.textViewNomePassageiro)
+        var textViewGeneroPassageiro = itemPassageiro.findViewById<TextView>(R.id.textViewGeneroPassageiro)
+        var textViewIdadePassageiro = itemPassageiro.findViewById<TextView>(R.id.textViewIdadePassageiro)
+
+
+        var passageiro : Passageiro? = null
+            get() = field
+            set(value) {
+                field = value
+
+                textViewNomePassageiro.text = passageiro?.nome ?: ""
+                textViewGeneroPassageiro.text = passageiro?.genero ?: ""
+                textViewIdadePassageiro.text = passageiro?.idade.toString()
         }
-    //var viewHolderSelecionado : View
 
+        init {
+            itemPassageiro.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            viewHolderSelecionado?.desseleciona()
+            seleciona()
+        }
+
+        private fun desseleciona() {
+            itemView.setBackgroundResource(android.R.color.white)
+        }
+
+        private fun seleciona() {
+            itemView.setBackgroundResource(android.R.color.holo_orange_light)
+            viewHolderSelecionado = this
+            fragment.passageiroSelecionado = passageiro
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderPassageiro {
+        val view = LayoutInflater.from(parent.context)
+        return ViewHolderPassageiro(view.inflate(R.layout.item_passageiro, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderPassageiro, position: Int) {
+        cursor!!.moveToPosition(position)
+        holder.passageiro = Passageiro.fromCursor(cursor!!)
+    }
+
+    override fun getItemCount(): Int {
+        if(cursor == null){
+            return 0
+        }
+        return cursor!!.count
+    }
 }
