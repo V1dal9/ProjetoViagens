89cf7c41323866ef4a5586716977e5f6ca52421b
Ana Vidal <raquelvidal99@hotmail.com>
2022-07-26 02:53:57 +0100

    Implementação do ContentProviderPassageiro.kt

--- app/src/main/java/com/example/projetoprogramacaoavancada/ContentProviderPassageiro.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/ContentProviderPassageiro.kt
@@ -2,16 +2,13 @@ package com.example.projetoprogramacaoavancada
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
+import android.provider.BaseColumns
 
 class ContentProviderPassageiro : ContentProvider() {
     var dbOpenHelper : BDViagemOpenHelper? = null
-    companion object{
-        private const val AUTHORITY = "com.example.projetoprogramacaoavancada"
-        private val ENDERCO_BASE = Uri.parse("content://$AUTHORITY")
-        val ENDERECO_PASSAGEIRO = Uri.withAppendedPath(ENDERCO_BASE, Tabela_Passageiro.NOME)
-    }
 
     override fun onCreate(): Boolean {
         dbOpenHelper = BDViagemOpenHelper(context)
@@ -33,20 +30,54 @@ class ContentProviderPassageiro : ContentProvider() {
         val argsSelecao = selectionArgs as Array<String>?
         val id = uri.lastPathSegment
 
-        return null
+        val cursor = when (ContentProviderViagem.getUriMatcher().match(uri)){
+            ContentProviderViagem.URI_PASSAGEIRO -> Tabela_Passageiro(db).query(colunas, selection, argsSelecao, null, null, sortOrder)
+            ContentProviderViagem.URI_PASSAGEIRO_ESPECIFICO -> Tabela_Passageiro(db).query(colunas, "${BaseColumns._ID}=?", arrayOf("${id}"), null,null, null)
+            else -> null
+        }
+        return cursor
 
     }
 
-    override fun getType(uri: Uri): String? {
-        TODO("Not yet implemented")
-    }
-
+    override fun getType(uri: Uri): String? =
+        when (getUriMatcher().match(uri)) {
+            ContentProviderViagem.URI_PASSAGEIRO -> "${ContentProviderViagem.MULTIPLOS_REGISTOS}/${Tabela_Passageiro.NOME}"
+            ContentProviderViagem.URI_PASSAGEIRO_ESPECIFICO -> "${ContentProviderViagem.UNICO_REGISTO}/${Tabela_Passageiro.NOME}"
+            else -> null
+        }
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        TODO("Not yet implemented")
-    }
+        val db = dbOpenHelper!!.writableDatabase
 
+        requireNotNull(values)
+
+        val id = when (ContentProviderViagem.getUriMatcher().match(uri)) {
+            URI_PASSAGEIRO -> Tabela_Passageiro(db).insert(values)
+            else -> -1
+        }
+
+        db.close()
+
+        if (id == -1L) return null
+
+        return Uri.withAppendedPath(uri, "$id")
+    }
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
-        TODO("Not yet implemented")
+        val db = dbOpenHelper!!.writableDatabase
+
+        val id = uri.lastPathSegment
+
+        val registosApagados = when (ContentProviderViagem.getUriMatcher().match(uri)) {
+            ContentProviderViagem.URI_LISTA_ESPECIFICA -> TabelaListaViagem(db).delete("${BaseColumns._ID}=?", arrayOf("${id}"))
+            ContentProviderViagem.URI_PASSAGEIRO_ESPECIFICO -> Tabela_Passageiro(db).delete("${BaseColumns._ID}=?", arrayOf("${id}"))
+            ContentProviderViagem.URI_COMPANHIA_ESPECIFICA -> Tabela_Companhia_Viagem(db).delete("${BaseColumns._ID} = ?", arrayOf("${id}"))
+            ContentProviderViagem.URI_BILHETE_ESPECIFICA -> Tabela_Info_Viagem_Bilhete(db).delete("${BaseColumns._ID} = ?", arrayOf("${id}"))
+            ContentProviderViagem.URI_LOCAL_ESPECIFICA -> Tabela_Info_Viagem_Bilhete(db).delete("${BaseColumns._ID} = ?", arrayOf("${id}"))
+            else -> 0
+        }
+
+        db.close()
+
+        return registosApagados
     }
 
     override fun update(
@@ -55,6 +86,41 @@ class ContentProviderPassageiro : ContentProvider() {
         selection: String?,
         selectionArgs: Array<out String>?
     ): Int {
-        TODO("Not yet implemented")
+        requireNotNull(values)
+
+        val db = dbOpenHelper!!.writableDatabase
+
+        val id = uri.lastPathSegment
+
+        val registosAlterados = when (ContentProviderViagem.getUriMatcher().match(uri)) {
+            URI_PASSAGEIRO_ESPECIFICO -> Tabela_Passageiro(db).update(values,"${BaseColumns._ID}=?", arrayOf("${id}"))
+            else -> 0
+        }
+
+        db.close()
+
+        return registosAlterados
     }
+
+
+
+
+    companion object{
+        private const val AUTHORITY = "com.example.projetoprogramacaoavancada"
+
+        private const val URI_PASSAGEIRO = 100
+        private const val URI_PASSAGEIRO_ESPECIFICO = 101
+
+        private val ENDERCO_BASE = Uri.parse("content://${AUTHORITY}")
+
+        val ENDERECO_PASSAGEIRO = Uri.withAppendedPath(ENDERCO_BASE, Tabela_Passageiro.NOME)
+        fun getUriMatcher() : UriMatcher{
+            val uriMatcher = UriMatcher(UriMatcher.NO_MATCH)
+
+            uriMatcher.addURI(AUTHORITY, Tabela_Passageiro.NOME, URI_PASSAGEIRO)
+            uriMatcher.addURI(AUTHORITY, "${Tabela_Passageiro.NOME}/#", URI_PASSAGEIRO_ESPECIFICO)
+            return uriMatcher
+        }
+    }
+
 }
